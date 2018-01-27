0a951c373353947b6aa4c3dddb68e9872b03a798
Kesh The Fire Lit God King <35199442+keshthegod@users.noreply.github.com>
2018-01-27 15:50:21 -0500

    Added Dishwasher Intent

--- WETApplication/app/src/main/java/com/wet/beastbots/wetapplication/FloorUsageActivity.java
+++ WETApplication/app/src/main/java/com/wet/beastbots/wetapplication/FloorUsageActivity.java
@@ -1,7 +1,11 @@
 package com.wet.beastbots.wetapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.wet.beastbots.R;
 
@@ -11,5 +15,14 @@ public class FloorUsageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_usage);
+        Button dishwasherButton= (Button) this.findViewById(R.id.dishwasher_button);
+        dishwasherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FloorUsageActivity.this, "WET Dishwasher Usage", Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(MainActivity.this, dishwasherButton.class);
+                //MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
