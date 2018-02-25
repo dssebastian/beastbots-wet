6cd67597dc4827be58d3692d7f9da13391fc92b3
ankithdagod <35199673+ankithdagod@users.noreply.github.com>
2018-02-25 14:26:27 -0500

    This is the final part of the app

---
+++ WETApplication/app/src/main/java/com/wet/beastbots/wetapplication/NameSensorsActivity.java
@@ -0,0 +1,21 @@
+package com.wet.beastbots.wetapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.wet.beastbots.R;
+
+public class NameSensorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_sensors);
+    }
+
+}
--- WETApplication/app/src/main/java/com/wet/beastbots/wetapplication/SetupActivity.java
+++ WETApplication/app/src/main/java/com/wet/beastbots/wetapplication/SetupActivity.java
@@ -1,11 +1,14 @@
 package com.wet.beastbots.wetapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
 import com.wet.beastbots.R;
 
 public class SetupActivity extends AppCompatActivity {
@@ -14,7 +17,45 @@ public class SetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        Button nameSensorButton = (Button) this.findViewById(R.id.name_sensors_button);
+        nameSensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, NameSensorsActivity.class);
+            }
+        });
+
+        Button nameFloorButton = (Button) this.findViewById(R.id.name_floor_button);
+        nameSensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, NameFloorsActivity.class);
+            }
+
+        });
+
+        Button sensorLocationButton = (Button) this.findViewById(R.id.sensor_location);
+        nameSensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SetupActivity.this, SensorLocationActivity.class);
+            }
+
+        });
+
 
     }
-
 }
